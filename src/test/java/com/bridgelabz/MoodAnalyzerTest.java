71561832a25a1c94f1d37a01e34b64fc6f4446a0
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-06 07:55:01 +0530

    Added :Enter Empty mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -9,21 +9,48 @@ public class MoodAnalyzerTest {
     @Test
     public void givenMessage_whenSad_ShouldReturnSad() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("SAD", mood);
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
     public void givenMessage_whenHappy_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any Mood");
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
     public void givenNullMood_ShouldReturnHappy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        String mood = moodAnalyzer.analyzeMood();
-        Assert.assertEquals("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenEmptyMood_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood;
+        try {
+            mood = moodAnalyzer.analyzeMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
